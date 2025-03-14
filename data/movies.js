7b1bea07c2cae350bff46ce3f7d618557e4b70ae
Patrik0601 <apatrik0601@gmail.com>
2025-03-14 11:25:57 +0100

    Kész

---
+++ data/movies.js
@@ -0,0 +1,8 @@
+const movies = [
+    {cim: "Star Wars", rendezo: "George Lucas", megjelnesEve: "1977", oszkardij: "igen"},
+    {cim: "Bosszúállók", rendezo: "Joss Whedon", megjelnesEve: "2012", oszkardij: "nem"},
+    {cim: "Oppenheimer", rendezo: "Christopher Nolan", megjelnesEve: "2023", oszkardij: "igen"},
+    {cim: "Barbie", rendezo: "Greta Gerwig", megjelnesEve: "2023", oszkardij: "nem"}
+]
+
+export default movies
